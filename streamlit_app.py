020191697da6c805269492091f73471641c30975
Qiusheng Wu <giswqs@gmail.com>
2024-10-25 11:06:01 -0400

    Add pre-commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,44 +5,48 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
-url = 'https://open.gishub.org/maxar-open-data'
-repo = 'https://github.com/opengeos/maxar-open-data/blob/master/datasets'
+url = "https://open.gishub.org/maxar-open-data"
+repo = "https://github.com/opengeos/maxar-open-data/blob/master/datasets"
 
-os.environ['GOOGLE_MAPS_API_KEY'] = 'API-KEY'
+os.environ["GOOGLE_MAPS_API_KEY"] = "API-KEY"
 m = leafmap.Map()
-m.add_basemap('SATELLITE')
-m.add_basemap('ROADMAP')
+m.add_basemap("SATELLITE")
+m.add_basemap("ROADMAP")
 
 
 @st.cache_data
 def get_datasets():
-    datasets = f'{url}/datasets.csv'
+    datasets = f"{url}/datasets.csv"
     df = pd.read_csv(datasets)
     return df
 
 
 @st.cache_data
 def get_catalogs(name):
-    dataset = f'{url}/datasets/{name}.tsv'
+    dataset = f"{url}/datasets/{name}.tsv"
 
-    dataset_df = pd.read_csv(dataset, sep='\t')
-    catalog_ids = dataset_df['catalog_id'].unique().tolist()
+    dataset_df = pd.read_csv(dataset, sep="\t")
+    catalog_ids = dataset_df["catalog_id"].unique().tolist()
     return catalog_ids
 
 
-st.title('Visualizing Maxar Open Data')
+st.title("Visualizing Maxar Open Data")
 
 col1, col2 = st.columns([1.2, 3.8])
 
 with col1:
-    default = 'Morocco-Earthquake-Sept-2023'
-    datasets = get_datasets()['dataset'].tolist()
-    dataset = st.selectbox('Select a dataset', datasets, index=datasets.index(default))
-    catalog = st.selectbox('Select a COG mosaic', get_catalogs(dataset), index=get_catalogs(dataset).index('10300500E4F91900'))
-    geojson = f'{url}/datasets/{dataset}.geojson'
-    mosaic = f'{url}/datasets/{dataset}/{catalog}.json'
-    tsv = f'{repo}/{dataset}/{catalog}.tsv'
-    st.markdown(f'View metadata: [{catalog}.tsv]({tsv})')
+    default = "Morocco-Earthquake-Sept-2023"
+    datasets = get_datasets()["dataset"].tolist()
+    dataset = st.selectbox("Select a dataset", datasets, index=datasets.index(default))
+    catalog = st.selectbox(
+        "Select a COG mosaic",
+        get_catalogs(dataset),
+        index=get_catalogs(dataset).index("10300500E4F91900"),
+    )
+    geojson = f"{url}/datasets/{dataset}.geojson"
+    mosaic = f"{url}/datasets/{dataset}/{catalog}.json"
+    tsv = f"{repo}/{dataset}/{catalog}.tsv"
+    st.markdown(f"View metadata: [{catalog}.tsv]({tsv})")
 
     with st.expander("Python code snippets"):
         markdown = f"""
@@ -56,15 +60,11 @@ m
         """
         st.code(markdown)
 
-
-    style = {
-        'weight': 1,
-        'fillOpacity': 0
-    }
-    m.add_geojson(geojson, layer_name=dataset, style=style, info_mode='on_click')
+    style = {"weight": 1, "fillOpacity": 0}
+    m.add_geojson(geojson, layer_name=dataset, style=style, info_mode="on_click")
     m.add_stac_layer(mosaic, name=catalog)
 
-    st.info('About')
+    st.info("About")
     markdown = f"""
     - [Web App Source Code](https://github.com/opengeos/maxar-open-data/blob/master/streamlit_app.py)
     - [GitHub Repo](https://github.com/opengeos/maxar-open-data)
